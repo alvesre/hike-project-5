bf55c7ec203a25fa95cd446446e3e9d0a895b870
Renata Alves <renata.alves@mobly.com.br>
2018-08-19 17:32:26 -0300

    Construir HTML e CSS.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Meu primeiro WebApp incrível' });
+});
+
+
+router.get('/helicopterson', function(req, res, next) {
+  res.render('bio');
 });
 
 module.exports = router;
