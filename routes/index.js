350679ccc690a30b1a57ffd64dbf6c9f97fa79f7
Renata Alves <renata.alves@mobly.com.br>
2018-08-20 14:11:19 -0300

    Criar substituições nos templates.

--- routes/index.js
+++ routes/index.js
@@ -1,42 +1,39 @@
 var express = require('express');
 var router = express.Router();
 
-
-const helicoptersonInfo = {
-	name: 'Helicopterson dos Santos',
-    role: 'Escritor, poeta, filantropo',
-    birth: '1870',
-    death: '1950',
-    background: 'Helicopterson sempre foi um jovem diferenciado, no mundo da lua, com aspirações muito além de sua geração. Com todos os seus sonhos e com muito esforço, um dia aprendeu a voar. Isso, ele aprendeu a voar, fazendo justiça à seu nome tão único. Ele continuou se esforçando, aprendendo muito e dominando a arte do voô, sempre chegando a alturas cada vez maiores. Até que um dia chegou na Lua, e essa na verdade é a última notícia que se tem dele. Depois disso, ele continuou voando cada vez mais alto, e ninguém mais o viu.',
-    photo: '/images/helicopterson.png',
-    quote: 'Se não for pra voar, eu nem desço pro play',
-};
-
-const lincolnInfo = {
-    name: 'Abraham Lincoln',
-    role: 'Ex-presidente, advogado',
-    birth: '1809',
-    death: '1865',
-    photo: '/images/lincoln.png',
-    background: 'Abraham Lincoln foi um político norte-americano que serviu como o 16° presidente dos Estados Unidos, posto que ocupou de 4 de março de 1861 até seu assassinato em 15 de abril de 1865. Lincoln liderou o país de forma bem-sucedida durante sua maior crise interna, a Guerra Civil Americana, preservando a União e abolindo a escravidão, fortalecendo o governo nacional. Lincoln tem sido consistentemente considerado por estudiosos e pelo povo como um dos três maiores presidentes dos Estados Unidos.',
-    quote: 'Governo das pessoas, pelas pessoas, para as pessoas, nunca deixará de existir na Terra.',
-    highlights: ['Aprovou a Lei da Propriedade Rural', 'Emitiu a Proclamação de Emancipação, que levou à abolição da escravatura nos EUA', 'Liderou a União à vitória da Guerra Civil'],
+const bios = {
+	helicopterson: {
+		name: 'Helicopterson dos Santos',
+	    role: 'Escritor, poeta, filantropo',
+	    birth: '1870',
+	    death: '1950',
+	    background: 'Helicopterson sempre foi um jovem diferenciado, no mundo da lua, com aspirações muito além de sua geração. Com todos os seus sonhos e com muito esforço, um dia aprendeu a voar. Isso, ele aprendeu a voar, fazendo justiça à seu nome tão único. Ele continuou se esforçando, aprendendo muito e dominando a arte do voô, sempre chegando a alturas cada vez maiores. Até que um dia chegou na Lua, e essa na verdade é a última notícia que se tem dele. Depois disso, ele continuou voando cada vez mais alto, e ninguém mais o viu.',
+	    photo: '/images/helicopterson.png',
+	    quote: 'Se não for pra voar, eu nem desço pro play',
+	    highlights: ['Nasceu', 'Voou', 'Voou mais alto', 'Primeiro homem a pisar na lua', 'Sumiu'],
+	},
+	lincoln: {
+		name: 'Abraham Lincoln',
+	    role: 'Ex-presidente, advogado',
+	    birth: '1809',
+	    death: '1865',
+	    photo: '/images/lincoln.png',
+	    background: 'Abraham Lincoln foi um político norte-americano que serviu como o 16° presidente dos Estados Unidos, posto que ocupou de 4 de março de 1861 até seu assassinato em 15 de abril de 1865. Lincoln liderou o país de forma bem-sucedida durante sua maior crise interna, a Guerra Civil Americana, preservando a União e abolindo a escravidão, fortalecendo o governo nacional. Lincoln tem sido consistentemente considerado por estudiosos e pelo povo como um dos três maiores presidentes dos Estados Unidos.',
+	    quote: 'Governo das pessoas, pelas pessoas, para as pessoas, nunca deixará de existir na Terra.',
+	    highlights: ['Aprovou a Lei da Propriedade Rural', 'Emitiu a Proclamação de Emancipação, que levou à abolição da escravatura nos EUA', 'Liderou a União à vitória da Guerra Civil'],
+	},
 };
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Meu primeiro WebApp incrível' });
+  res.render('index', { bios: bios });
 });
 
 
-/* GET Helicopterson's bio. */
-router.get('/helicopterson', function(req, res, next) {
-  res.render('bio', helicoptersonInfo);
-});
-
-/* GET Lincoln's bio. */
-router.get('/lincoln', function(req, res, next) {
-  res.render('bio', lincolnInfo);
+/* GET Bios. */
+router.get('/:id', function(req, res, next) {
+  const id = req.params.id;
+  res.render('bio', bios[id]);
 });
 
 module.exports = router;
