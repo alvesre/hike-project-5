bcd75120c7218e664b54a309b7eadd3b1828bcd9
Renata Alves <renata.alves@mobly.com.br>
2018-08-20 23:29:13 -0300

    Finish

--- routes/index.js
+++ routes/index.js
@@ -24,13 +24,23 @@ const bios = {
 	},
 	gatilson: {
 		name: 'Gatilson de Souza',
-	    role: 'Ser fofíneo, companheiríneo',
+	    role: 'Ser fofíneo, não se importar com nada que acontece ao seu redor',
 	    birth: '2015',
-	    death: 'vivo',
+	    death: 'até hoje',
 	    photo: '/images/gatilson.png',
-	    background: 'Responsável por subir nos móveis, derrubar objetos de decoração e ignorar seus donos como se fosse a última bolacha do pacote.',
+	    background: 'Responsável por subir nos móveis, derrubar objetos de decoração e ignorar seus donos, mas morrer de amor por eles. Provocar o cachorro.',
 	    quote: 'Gatos amam mais as pessoas do que elas permitiriam. Mas eles têm sabedoria suficiente para manter isso em segredo.',
 	    highlights: ['Os gatos têm cerca de 100 sons vocais ao contrário dos cães que apresentam apenas 10', 'Um gato pode ficar vários dias sem comer, caso o alimento não lhe agrada, e pode até morrer de fome.', 'Os felinos podem chegar a dormir até 16 horas por dia, divididas em vários períodos. Isto quer dizer que um gato de 7 anos só esteve realmente acordado durante dois anos.', 'O gato tem 245 ossos, enquanto que o homem tem 206 no total.', 'Quando o gato está assustado, seu pelo se eriça por todo o corpo. Quando ele vai atacar, o pelo se eriça apenas numa estreita faixa dorsal.', 'O gato pode alcançar uma velocidade de até 50 km/h.', 'Os gatos esfregam o rosto em objetos e pessoas para marcá-los com seu cheiro. O cheiro é produzido por glândulas localizadas na parte anterior do rosto.'],
+	},
+	catioro: {
+		name: 'Catioro Silva',
+	    role: 'Ser companheiríneo',
+	    birth: '2007',
+	    death: '2010',
+	    photo: '/images/catioro.png',
+	    background: 'Ser o melhor amigo em todas as horas, amar seu dono acima de tudo, latir para o gato.',
+	    quote: 'Um cão é a única coisa na terra que o ama mais do que a si mesmo.',
+	    highlights: ['O olfato dos cachorros é 1 milhão de vezes melhor do que o dos seres humanos. O olfato dos cães é um dos melhores da natureza. Se as membranas situadas no nariz dos cães fossem estendidas, elas seriam maiores que o próprio cão.', 'A audição dos cachorros é 10 vezes melhor do que a dos humanos.', 'Castrar seu cachorro pode ajudar a prevenir vários tipos de câncer. Veja aqui as vantagens da castração.', 'Se não for castrada, uma cachorra pode ter 66 filhotes em 6 anos', 'Um cão pode correr até 30km/h. A raça mais rápida do mundo é o Whippet.', 'Na Bíblia, os cachorros são mencionados 14 vezes. ', 'As cachorras carregam seus bebês na barriga por 60 dias antes deles nasceram'],
 	},
 };
 
